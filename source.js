282b1779f4697dda9bf5768cf04cd5cea938f24d
BettyBeiZhang <beizhang18@yahoo.com>
2017-01-29 04:08:49 -0800

    [create] project& experience page, add font-awesome css

--- source.js
+++ source.js
@@ -11,12 +11,12 @@
   });
 
   $(window).scroll(function() {
-    $('#linkbtn').each(function(){
+    $('.linkbtn').each(function(){
     var imagePos = $(this).offset().top;
 
     var topOfWindow = $(window).scrollTop();
       if (imagePos < topOfWindow+400) {
-        $(this).addClass("floating");
+        $(this).addClass("tossing");
       }
     });
   });
