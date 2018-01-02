bd5ff910581c9c1ea328c8e8d514902d785a45dc
bei zhang <beizhang18@yahoo.com>
2018-01-01 22:37:44 -0800

    update responsive navbar

--- source.js
+++ source.js
@@ -1,25 +1,37 @@
+$(window).scroll(function() {
+  $('#myphoto').each(function(){
+  var imagePos = $(this).offset().top;
 
-  $(window).scroll(function() {
-    $('#myphoto').each(function(){
-    var imagePos = $(this).offset().top;
-
-    var topOfWindow = $(window).scrollTop();
-      if (imagePos < topOfWindow+400) {
-        $(this).addClass("fadeIn");
-      }
-    });
+  var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow+400) {
+      $(this).addClass("fadeIn");
+    }
   });
+});
 
-  $(window).scroll(function() {
-    $('.linkbtn').each(function(){
-    var imagePos = $(this).offset().top;
+// $(window).scroll(function() {
+//   $('.linkbtn').each(function(){
+//   var imagePos = $(this).offset().top;
 
-    var topOfWindow = $(window).scrollTop();
-      if (imagePos < topOfWindow+400) {
-        $(this).addClass("tossing");
-      }
+//   var topOfWindow = $(window).scrollTop();
+//     if (imagePos < topOfWindow+400) {
+//       $(this).addClass("tossing");
+//     }
+//   });
+// });
+
+$(".nav-link").on('click', function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+    var hash = this.hash;
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 800, function(){
+      window.location.hash = hash;
     });
-  });
+  }
+});
 
 
- 
+
+
